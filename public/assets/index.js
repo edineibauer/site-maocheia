8cab8cd3ad8443268fdb856b9a1d5eb2e160f482
Edinei J. Bauer <galera.org@gmail.com>
2020-03-05 22:31:35 -0300

    index page

--- public/assets/index.js
+++ public/assets/index.js
@@ -1,7 +1,35 @@
-if(HOMEPAGE === "1") {
-    if (getCookie("token") !== "" && getCookie("token") !== "0") {
-        pageTransition("dashboard", "route", "forward", "#core-content", null, null, !1);
-    } else {
-        pageTransition("login", "route", "forward", "#core-content", null, null, !1);
-    }
-}
+$(document).ready(function () {
+    $('#apresentacao .owl-carousel').owlCarousel({
+        loop: false,
+        margin: 10,
+        nav: false,
+        autoplay: false,
+        dots: true,
+        responsive: {
+            0: {
+                items: 1
+            },
+        }
+    });
+
+    // Go to the next item
+    var owl = $('.owl-carousel');
+    owl.owlCarousel();
+
+    owl.on('translated.owl.carousel', function (e) {
+        $('.btn-proximo').prop('disabled', true);
+        if (e.page.count === e.page.index + 1) {
+            $('.btn-proximo').prop('disabled', false).text('Pular').off("click").on('click', function () {
+                $("#goHome").trigger("click");
+            });
+        } else {
+            $('.btn-proximo').prop('disabled', false).text('Próximo').off("click").on("click", function () {
+                owl.trigger('next.owl.carousel');
+            });
+        }
+    });
+
+    $('.btn-proximo').off("click").on("click", function () {
+        owl.trigger('next.owl.carousel');
+    });
+});
