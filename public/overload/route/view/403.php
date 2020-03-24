9799b0e020d379a1edcd077498fb695f89465cc0
Edinei J. Bauer <galera.org@gmail.com>
2020-03-24 18:36:18 -0300

    notificações e mensagem fix image, view 403 e network overloaded

---
+++ public/overload/route/view/403.php
@@ -0,0 +1,31 @@
+<section id="criar-conta">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="perfil" data-animation="back" class="volta">
+                    <i class="material-icons">arrow_back</i>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div>
+                    <h3 class="titulo">Sem permissão</h3>
+                </div>
+                <div class="texto mb-4 mt-5 text-center">
+                    seu usuário não tem permissão para acessar este caminho!
+                </div>
+                <div class="text-center col">
+                    <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" fill="#2D7AFF" version="1.1" width="24" height="24" viewBox="0 0 24 24"><path d="M12,17A2,2 0 0,0 14,15C14,13.89 13.1,13 12,13A2,2 0 0,0 10,15A2,2 0 0,0 12,17M18,8A2,2 0 0,1 20,10V20A2,2 0 0,1 18,22H6A2,2 0 0,1 4,20V10C4,8.89 4.9,8 6,8H7V6A5,5 0 0,1 12,1A5,5 0 0,1 17,6V8H18M12,3A3,3 0 0,0 9,6V8H15V6A3,3 0 0,0 12,3Z" /></svg>
+                </div>
+            </div>
+
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-5 col-md-3 col-lg-4 mt-4">
+                <div class="botao">
+                    <a href="#back" class="btn btn-primary">voltar</a>
+                </div>
+            </div>
+        </div>
+</section>
---
+++ public/overload/route/view/network.php
@@ -0,0 +1,31 @@
+<section id="criar-conta">
+    <div class="container">
+        <div class="row">
+            <div class="col-12">
+                <a href="perfil" data-animation="back" class="volta">
+                    <i class="material-icons">arrow_back</i>
+                </a>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col-12">
+                <div>
+                    <h3 class="titulo">Sem Conexão</h3>
+                </div>
+                <div class="texto mb-4 mt-5 text-center">
+                    volte quando estiver conectado novamente!
+                </div>
+                <div class="text-center col">
+                    <img src="<?= HOME ?>assetsPublic/img/nonetwork.svg?v=<?= VERSION ?>">
+                </div>
+            </div>
+
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-5 col-md-3 col-lg-4 mt-4">
+                <div class="botao">
+                    <a href="#back" class="btn btn-primary">voltar</a>
+                </div>
+            </div>
+        </div>
+</section>
