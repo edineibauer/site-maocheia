8efb2789e21d6d0ff7ea24a236b609347636083f
Edinei J. Bauer <galera.org@gmail.com>
2020-04-20 11:11:28 -0300

    fix notificacoes front

--- public/assets/notificacoes.js
+++ public/assets/notificacoes.js
@@ -1,30 +1,35 @@
-$(function() {
-
-    if(Notification.permission !== "default")
+$(function () {
+    if (Notification.permission !== "default")
         $(".btn-notify").remove();
 
-    db.exeRead("notifications").then(notifications => {
+    db.exeRead("notifications_report").then(notifications => {
         let myNotifications = [];
+        let promessas = [];
         if (!isEmpty(notifications)) {
             for (let i in notifications) {
                 if (notifications[i].usuario == USER.id) {
-                    notifications[i].calendar = moment(notifications[i].data).calendar().toLowerCase();
-                    myNotifications.push(notifications[i]);
+                    promessas.push(db.exeRead("notifications", notifications[i].notificacao).then(notify => {
+                        notify.data = moment(notifications[i].data_de_envio).calendar().toLowerCase();
+                        notifications[i].notificacaoData = notify;
+                        myNotifications.push(notifications[i]);
+                    }));
                 }
             }
         }
 
-        if (isEmpty(myNotifications)) {
-            $("#notificacoes").htmlTemplate('notificacoesEmpty');
-        } else {
-            $("#notificacoes").htmlTemplate('cardsNotificacoes', {notificacoes: myNotifications}).then(() => {
-                $(".notification-card").off("click").on("click", function () {
-                    let href = $(this).data("href");
-                    let id = parseInt($(this).attr("rel"));
-                    db.exeDelete("notifications", id);
-                    pageTransition(href);
+        Promise.all(promessas).then(() => {
+            if (isEmpty(myNotifications)) {
+                $("#notificacoes").htmlTemplate('notificacoesEmpty');
+            } else {
+                $("#notificacoes").htmlTemplate('cardsNotificacoes', {notificacoes: myNotifications}).then(() => {
+                    $(".notification-card").off("click").on("click", function () {
+                        let href = $(this).data("href");
+                        let id = parseInt($(this).attr("rel"));
+                        db.exeDelete("notifications", id);
+                        pageTransition(href);
+                    });
                 });
-            });
-        }
+            }
+        });
     });
 });
