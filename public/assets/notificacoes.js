068d32c2f2daca73c7800c73f63450f895f2aa03
Edinei J. Bauer <galera.org@gmail.com>
2020-04-20 22:19:48 -0300

    notificaçoes

--- public/assets/notificacoes.js
+++ public/assets/notificacoes.js
@@ -2,34 +2,13 @@ $(function () {
     if (Notification.permission !== "default")
         $(".btn-notify").remove();
 
-    db.exeRead("notifications_report").then(notifications => {
-        let myNotifications = [];
-        let promessas = [];
-        if (!isEmpty(notifications)) {
-            for (let i in notifications) {
-                if (notifications[i].usuario == USER.id) {
-                    promessas.push(db.exeRead("notifications", notifications[i].notificacao).then(notify => {
-                        notify.data = moment(notifications[i].data_de_envio).calendar().toLowerCase();
-                        notifications[i].notificacaoData = notify;
-                        myNotifications.push(notifications[i]);
-                    }));
-                }
-            }
+    (async () => {
+        let myNotifications = await getNotifications();
+        if (isEmpty(myNotifications)) {
+            $("#notificacoes").htmlTemplate('notificacoesEmpty');
+        } else {
+            console.log(myNotifications);
+            $("#notificacoes").htmlTemplate('note', {notificacoes: myNotifications});
         }
-
-        Promise.all(promessas).then(() => {
-            if (isEmpty(myNotifications)) {
-                $("#notificacoes").htmlTemplate('notificacoesEmpty');
-            } else {
-                $("#notificacoes").htmlTemplate('cardsNotificacoes', {notificacoes: myNotifications}).then(() => {
-                    $(".notification-card").off("click").on("click", function () {
-                        let href = $(this).data("href");
-                        let id = parseInt($(this).attr("rel"));
-                        db.exeDelete("notifications", id);
-                        pageTransition(href);
-                    });
-                });
-            }
-        });
-    });
+    })();
 });
