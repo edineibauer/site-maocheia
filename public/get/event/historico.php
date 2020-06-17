9cf6612a29deae4bf3406ec2e13b7a464dabe02b
Edinei J. Bauer <galera.org@gmail.com>
2020-06-17 18:26:25 -0300

    imagem perfil historico

--- public/get/event/historico.php
+++ public/get/event/historico.php
@@ -11,6 +11,7 @@ $data['data'] = [];
 $sql->exeCommand("SELECT *, 1 as isProfissional, h.id as id FROM " . PRE . "historico as h JOIN " . PRE . "clientes as c ON c.id = h.cliente WHERE h.profissional = {$_SESSION['userlogin']['setorData']['id']} && h.cliente != {$_SESSION['userlogin']['setorData']['id']} ORDER BY h.data DESC LIMIT 50");
 if($sql->getResult()) {
     foreach ($sql->getResult() as $item) {
+        $item['perfil_profissional'] = (!empty($item['perfil_profissional']) ? json_decode($item['perfil_profissional'], !0)[0] : []);
         $read->exeRead("atendimento", "WHERE cliente = :c AND autorpub = :p", "c={$item['cliente']}&p={$_SESSION['userlogin']['id']}");
         $item['avaliou'] = $read->getResult() ? !0 : !1;
         $data['data'][] = $item;
