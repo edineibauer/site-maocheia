5bc0ffa9cf648fb7d3f5a0c974c1784a56153836
Edinei J. Bauer <galera.org@gmail.com>
2020-07-22 16:03:22 -0300

    up mensagem and perfil usuário

--- public/overload/chat/view/mensagem/mensagemMenu.js
+++
@@ -1,7 +0,0 @@
-(async () => {
-    let cliente = await AJAX.getUrl(SERVER + "app/find/clientes/usuarios_id/" + history.state.param.url[0]);
-    if(cliente.response === 1 && !isEmpty(cliente.data) && !isEmpty(cliente.data.clientes)) {
-        cliente = cliente.data.clientes[0];
-        $("#menu-chat").htmlTemplate("mensagemMenu", cliente);
-    }
-})();
--- public/view/perfilUsuario/cliente.js
+++ public/view/perfilUsuario/cliente.js
@@ -1,13 +1,15 @@
 $(function () {
     (async () => {
-
-        let cliente = await db.exeRead("clientes", history.state.param.url[0]);
+        let cliente = await read.exeRead("clientes", {"usuarios_id": history.state.param.url[0]});
         if(isEmpty(cliente)) {
-            toast("usuário não encontrado", 3000, "toast-info");
+            toast("usuário não encontrado", 1000, "toast-infor");
             history.back();
             return;
+        } else {
+            cliente = cliente[0];
         }
 
+        cliente.home = HOME;
         cliente.imagem_url = (!isEmpty(cliente.perfil_profissional) && !isEmpty(cliente.perfil_profissional[0].imagem_de_perfil) ? cliente.perfil_profissional[0].imagem_de_perfil[0].urls.medium : (!isEmpty(cliente.imagem) ? cliente.imagem[0].urls.medium : ""));
         let avaliacao_geral = parseFloat(parseFloat((!isEmpty(cliente.perfil_profissional) ? ((cliente.perfil_profissional[0].atendimento + cliente.perfil_profissional[0].qualidade) / 2) : cliente.avaliacao) / 10000000).toFixed(1));
         cliente.avaliacao = getProfissionalStar(avaliacao_geral);
@@ -34,7 +36,7 @@ $(function () {
             }
 
         } else {
-            $("#section-avaliacoes-title").html("Nenhuma avaliação");
+            $("#section-avaliacoes-title").html("Nenhuma avaliação").addClass("text-center");
         }
 
         $("#section-avaliacoes").html(Mustache.render(tpl.avaliacoes, feedbacks));
