9e4f752f1909ee608001df8136b0270f8a231b54
Edinei J. Bauer <galera.org@gmail.com>
2020-07-23 13:54:18 -0300

    open chat

--- public/set/openMessage.php
+++ public/set/openMessage.php
@@ -3,56 +3,33 @@
 $id = filter_input(INPUT_POST, 'id', FILTER_VALIDATE_INT);
 
 $read = new \Conn\Read();
-$read->exeRead("mensagens", "WHERE id = :id", "id={$id}");
+$read->exeRead("clientes", "WHERE id = :pid && ativo = 1", "pid={$_SESSION['userlogin']['setorData']['id']}");
 if ($read->getResult()) {
-    $mensagem = $read->getResult()[0];
+    $prof = $read->getResult()[0];
+    if (!empty($prof['moedas']) && is_numeric($prof['moedas']) && $prof['moedas'] > 1) {
+        $up = new \Conn\Update();
+        $_SESSION['userlogin']['setorData']['moedas'] = ($prof['moedas'] - 1);
 
-    if ($mensagem['aceitou'] == 1) {
-        $data = [
-            "response" => 2,
-            "data" => "",
-            "error" => "Já esta liberado, recarregue a página"
-        ];
+        $up->exeUpdate("clientes", ["moedas" => $_SESSION['userlogin']['setorData']['moedas']], "WHERE id = :id", "id={$_SESSION['userlogin']['setorData']['id']}");
+//        $up->exeUpdate("messages_user", ["aceito" => 1], "WHERE usuario = :u", "u={$id}");
+
+        $f = fopen(PATH_HOME . "_cdn/userPerfil/" . $_SESSION['userlogin']['id'] . ".json", "w+");
+        fwrite($f, json_encode($_SESSION['userlogin']));
+        fclose($f);
+
+        $data['data'] = 1;
+
     } else {
-
-        if ($mensagem['profissional'] == $_SESSION['userlogin']['setorData']['id']) {
-
-            $read->exeRead("clientes", "WHERE id = :pid && ativo = 1", "pid={$_SESSION['userlogin']['setorData']['id']}");
-            if ($read->getResult()) {
-                $prof = $read->getResult()[0];
-                if (!empty($prof['moedas']) && is_numeric($prof['moedas']) && $prof['moedas'] > 1) {
-                    $up = new \Conn\Update();
-                    $up->exeUpdate("clientes", ["moedas" => ($prof['moedas'] - 1)], "WHERE id = :id", "id={$_SESSION['userlogin']['setorData']['id']}");
-                    $up->exeUpdate("mensagens", ["aceitou" => 1], "WHERE id = :id", "id={$id}");
-                    $data['data'] = 1;
-                } else {
-                    $data = [
-                        "response" => 3,
-                        "data" => HOME . "comprar_moedas",
-                        "error" => ""
-                    ];
-                }
-            } else {
-                $data = [
-                    "response" => 2,
-                    "data" => "",
-                    "error" => "Profissional não encontrado, faça login antes"
-                ];
-            }
-
-        } else {
-            $data = [
-                "response" => 2,
-                "data" => "",
-                "error" => "Você só pode solicitar liberação de conversas do seu perfil"
-            ];
-        }
+        $data = [
+            "response" => 3,
+            "data" => HOME . "comprar_moedas",
+            "error" => ""
+        ];
     }
-
 } else {
     $data = [
         "response" => 2,
         "data" => "",
-        "error" => "Id de mensagem não encontrado"
+        "error" => "Profissional não encontrado, faça login antes"
     ];
 }
--- public/view/mensagem/coin.js
+++ public/view/mensagem/coin.js
@@ -1,7 +1,22 @@
-(async () => {
-    let busca = new Read;
-    let minhaMensagem = await busca.exeRead("messages_user", {"usuario": history.state.param.url[0]});
-    if(isEmpty(minhaMensagem.mensagem)) {
-        console.log('ok');
-    }
-})();
+$(function () {
+    (async () => {
+        let minhaMensagem = await exeRead("messages_user", {"usuario": history.state.param.url[0]});
+        let tpl = await getTemplates();
+        if(isEmpty(minhaMensagem[0].mensagem)) {
+            $("#frame").append(Mustache.render(tpl.coin));
+        } else {
+            $(".message-input").css("display", "block");
+        }
+
+        $("#app").off("click", ".btn-buy").on("click", ".btn-buy", function () {
+            $(".message-input").css("display", "block");
+            $(".message-input-buy").remove();
+            AJAX.post("openMessage", {id: history.state.param.url[0]}).then(r => {
+                console.log(r);
+                if(r === 1) {
+                    USER.setorData.moeda--;
+                }
+            });
+        });
+    })();
+});
